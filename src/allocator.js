21fdc728613344bc38139c9ea8e254f6ff85783f
adelin <boovad@live.com>
2017-05-12 20:14:03 +0300

    Create Allocator

---
+++ src/allocator.js
@@ -0,0 +1,12 @@
+class Allocator {
+    /**
+     * @param {Object} campaign
+     *
+     * @return {Allocator}
+     */
+    constructor(campaign) {
+        this.$campaign = campaign;
+    }
+}
+
+export default Allocator;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
+import config from './config';
 import add_assets from './utils/asset';
+import ajax from './utils/ajax';
+import Allocator from './allocator';
 
 add_assets()
     .then(() => {
-        console.log('do work..');
+        let uri = config.app_path + '/pbjs/campaign/';
+
+        ajax().json(uri)
+            .then((response) => {
+                const allocator = new Allocator(response.text);
+            })
+            .catch((e) => {
+                console.error(e);
+            });
     });
