32e6a1db4eae8672c8e446573258d8a80200bcf9
adelin <boovad@live.com>
2017-05-12 20:19:14 +0300

    Add Auction
    - setup prebidjs auction
    - create custom timeouts - mobile vs desktop
    - create offers listener in Allocator

--- src/allocator.js
+++ src/allocator.js
@@ -1,3 +1,5 @@
+import Auction from './auction';
+
 class Allocator {
     /**
      * @param {Object} campaign
@@ -6,6 +8,57 @@ class Allocator {
      */
     constructor(campaign) {
         this.$campaign = campaign;
+
+        this.$auction = new Auction(this);
+
+        this.$offers = [];
+    }
+
+    /**
+     * Offers listener - called by Auction
+     *
+     * @param {Object} offers
+     *
+     * @return {Allocator}
+     */
+    offersListener(offers) {
+        try {
+            // add main bids
+            for (const target_id in offers) {
+                const targetOffers = offers[target_id];
+
+                if (!this.$offers[target_id]) {
+                    this.$offers[target_id] = [];
+                }
+
+                targetOffers.bids.forEach((bid) => {
+                    if (bid.getStatusCode() !== 1) {
+                        return false;
+                    }
+
+                    this.$offers[target_id].push(bid);
+                });
+            }
+
+            // sort bids
+            for (const target_id in this.$offers) {
+                this.$offers[target_id].sort((a, b) => {
+                    return b.cpm - a.cpm;
+                });
+
+                const ad = this.$offers[target_id].shift();
+                if (!ad) {
+                    // add backup
+                    console.warn(target_id, 'requires a backup');
+                }
+
+                console.log(target_id, ad);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+
+        return this;
     }
 }
 
---
+++ src/auction.js
@@ -0,0 +1,107 @@
+import config from './config';
+import device from './utils/device';
+
+class Auction {
+    /**
+     * @param {Alocator} allocator
+     *
+     * @return {Auction}
+     */
+    constructor(allocator) {
+        this.__allocator = allocator;
+
+        this.$pbjs = window.pbjs = window.pbjs || {};
+        this.$pbjs.que = this.$pbjs.que || [];
+
+        this.$adUnits = [];
+
+        this.determineUnits()
+            .addUnits();
+    }
+
+    /**
+     * Request bids
+     *
+     * @return {Auction}
+     */
+    addUnits() {
+        const device_key = device.mobile() ? 'mobile' : 'desktop',
+            timeout = config.timeout[device_key] * 1000;
+
+        pbjs.que.push(() => {
+            pbjs.addAdUnits(this.$adUnits);
+
+            pbjs.requestBids({
+                timeout: timeout,
+                bidsBackHandler: (codes) => {
+                    this.__allocator.offersListener(codes);
+                }
+            });
+        });
+
+        return this;
+    }
+
+    /**
+     * @return {Auction}
+     */
+    determineUnits() {
+        const units = {};
+        const added = {
+            sizes: [],
+            bidders: []
+        };
+        let reference_key;
+
+        // units: create as object for uniqueness
+        this.__allocator.$campaign.providers.forEach((provider) => {
+            for (const target_id in this.__allocator.$campaign.targets) {
+                this.__allocator.$campaign.targets[target_id].forEach((size) => {
+                    if (provider.available_sizes.indexOf(size) === -1) {
+                        return false;
+                    }
+
+                    if (!units[target_id]) {
+                        units[target_id] = {
+                            code: target_id
+                        };
+                    }
+
+                    reference_key = `${target_id}_${size}`;
+                    if (added.sizes.indexOf(reference_key) === -1) {
+                        added.sizes.push(reference_key);
+
+                        if (!units[target_id].sizes) {
+                            units[target_id].sizes = [];
+                        }
+
+                        units[target_id].sizes.push(size.split('x'));
+                    }
+
+                    reference_key = `${target_id}_${provider.bidder}`;
+                    if (added.bidders.indexOf(reference_key) === -1) {
+                        added.bidders.push(reference_key);
+
+                        if (!units[target_id].bids) {
+                            units[target_id].bids = [];
+                        }
+
+                        const cleanedProvider = Object.assign({}, provider);
+                        delete cleanedProvider.available_sizes;
+
+                        units[target_id].bids.push(cleanedProvider);
+                    }
+                });
+            }
+        });
+
+        // units: to array
+        for (const target_id in units) {
+            this.$adUnits.push(units[target_id]);
+        }
+
+        return this;
+    }
+}
+
+export default Auction;
